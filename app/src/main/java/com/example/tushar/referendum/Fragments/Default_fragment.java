91f7b6f18cde2223ef5eaa87ce8f1676ed097aa7
Tushar Mann <tusharmann29@gmail.com>
2016-08-15 21:15:56 +0530

    referendum app introduced

---
+++ app/src/main/java/com/example/tushar/referendum/Fragments/Default_fragment.java
@@ -0,0 +1,29 @@
+package com.example.tushar.referendum.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tushar.referendum.R;
+
+public class Default_fragment extends Fragment {
+
+    public Default_fragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_default_fragment, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+}
