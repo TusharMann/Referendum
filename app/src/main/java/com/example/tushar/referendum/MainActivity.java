d2c86a287b0287c3fddb140ef5ae6ed52ba6e424
TusharMann <tusharmann29@gmail.com>
2016-12-21 12:46:46 +0530

    pdf renderer

--- app/src/main/java/com/example/tushar/referendum/MainActivity.java
+++ app/src/main/java/com/example/tushar/referendum/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.tushar.referendum;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        render();
+
     }
+
+    private void render(){
+        try {
+            image = (ImageView) findViewById(R.id.image);
+            int Req_Wid = image.getWidth();
+            int Req_Hei = image.getHeight();
+
+            Bitmap bitmap = Bitmap.createBitmap(Req_Wid, Req_Hei, Bitmap.Config.ARGB_4444);
+            File file = new File("/Tushar_resume (1).pdf");
+
+            PdfRenderer renderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
+            Matrix m = image.getImageMatrix();
+            Rect rect = new Rect(0, 0, Req_Wid, Req_Hei);
+            renderer.openPage(1).render(bitmap, rect, m, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            image.setImageMatrix(m);
+            image.setImageBitmap(bitmap);
+            image.invalidate();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
